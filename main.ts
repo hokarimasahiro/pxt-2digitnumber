8f79f71f9d148a7d3955b85d771ca64e259d9ce7
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2020-03-20 09:21:08 +0900

    Updates.

--- main.ts
+++ main.ts
@@ -7,7 +7,7 @@ namespace show2DigitNumber {
      * show 2-digit number
      * @param n number, eg: 17
      */
-    //% blockId="showNumber" block="showNumber %n"
+    //% blockId=showNumber block="showNumber %n"
     export function showNumber(n: number): void {
         const font: number[] = [1023, 31, 765, 703, 927, 951, 1015, 636, 891, 959]
         let dfont: number
